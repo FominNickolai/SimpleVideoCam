820cb589c8480cda3a1bb32451294e2e706410bb
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-01 20:18:40 +0300

    Initial Commit

---
+++ SimpleVideoCam/SimpleVideoCamController.swift
@@ -0,0 +1,37 @@
+//
+//  SimpleVideoCamController.swift
+//  SimpleVideoCam
+//
+//  Created by Simon Ng on 17/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class SimpleVideoCamController: UIViewController {
+
+    @IBOutlet var cameraButton:UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Action methods
+    
+    @IBAction func unwindToCamera(segue:UIStoryboardSegue) {
+        
+    }
+    
+    @IBAction func capture(sender: AnyObject) {
+    }
+
+
+}
