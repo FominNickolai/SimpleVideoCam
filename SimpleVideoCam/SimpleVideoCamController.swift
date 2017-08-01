c224df609945d38fba57290419365525d7d38b56
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-01 21:47:08 +0300

    Add Privacy into info.plist. Create AVCAptureSession. Set default device to video capture and add it to view.layer.

--- SimpleVideoCam/SimpleVideoCamController.swift
+++ SimpleVideoCam/SimpleVideoCamController.swift
@@ -7,15 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SimpleVideoCamController: UIViewController {
 
     @IBOutlet var cameraButton:UIButton!
+    let captureSession = AVCaptureSession()
+    
+    var videoFileOutput: AVCaptureMovieFileOutput?
+    
+    var currentDevice: AVCaptureDevice?
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Preset the session for taking photo in full resolution
+        captureSession.sessionPreset = AVCaptureSessionPresetHigh
 
-        // Do any additional setup after loading the view.
+        //Get the default camera
+        let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        currentDevice = device
+        
+        //Get the input data source
+        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: device) else { return }
+        
+        videoFileOutput = AVCaptureMovieFileOutput()
+        
+        //Configure the session with the input and the output devices
+        captureSession.addInput(captureDeviceInput)
+        captureSession.addOutput(videoFileOutput)
+        
+        //Provide a camera preview
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(cameraPreviewLayer!)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        cameraPreviewLayer?.frame = view.layer.frame
+        
+        //Bring the camera button to front
+        view.bringSubview(toFront: cameraButton)
+        captureSession.startRunning()
+        
     }
 
     override func didReceiveMemoryWarning() {
